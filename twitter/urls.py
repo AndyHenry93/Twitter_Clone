3d456fc07294423cea36d0bd57c78a5afb22a89a
andy henry <a.henry250@gmail.com>
2023-05-18 14:01:13 -0400

    finished user profiles

--- twitter/urls.py
+++ twitter/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('profile_list/',views.profile_list,name='profile_list'),
     path('signin/',views.signin,name="signin"),
-    path('signout/',views.signout,name="signout")
+    path('signout/',views.signout,name="signout"),
+    path('profile/<int:id>/',views.profile,name="profile"),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . models import Profile
 from . forms import LoginForm
 from django.contrib.auth import login, authenticate,logout
 from django.contrib import messages
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
     return render(request,'twitter/home.html')
-
 
 def profile_list(request):
     profile = Profile.objects.exclude(user=request.user)
@@ -27,7 +27,8 @@ def signin(request):
                 login(request,user)
                 return redirect('twitter:home')
             else:
-                return HttpResponse("Invalid username or password")
+                messages.error(request,"Invalid username or password, Please try again.")
+                return redirect("twitter:home")
     else:
         user_signin = LoginForm()
         return render(request,'twitter/signin.html',{"user_signin":user_signin})
@@ -35,6 +36,30 @@ def signin(request):
 def signout(request):
     logout(request)
     return redirect("twitter:home")
+
+def profile(request,id):
+    if request.user.is_authenticated:
+        user_profile = get_object_or_404(Profile,id=id)
+        # form logic
+        if request.method == "POST":
+            # get current user
+            curr_user = request.user.profile
+            # get form data
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action == "unfollow":
+                curr_user.follow.remove(user_profile)
+            else:
+                curr_user.follow.add(user_profile)
+            curr_user.save()
+        context = {
+            "user_profile":user_profile
+        }
+        return render(request,'twitter/profile.html',context)
+    else:
+        messages.error(request,"you must be logged in to access this page")
+        return redirect("twitter:home")
+
 
 
 
